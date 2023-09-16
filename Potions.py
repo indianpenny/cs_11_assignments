84bea21faac89a9d948e0a4276649f33cdf485e4
indianpenny <indianpenny28@gmail.com>
2023-09-16 13:47:20 -0500

    Updated the program for easy to read.

--- Potions.py
+++ Potions.py
@@ -7,22 +7,22 @@ Created on Feb 1 15:37:20 2023
 #Potions Homework code by Srishti M. 
 
 n = int(input().strip())
+
 if not (1 <= n <= 100000):
     print("Invalid input values")
     exit()
 
 task_difficulty = [int(input().strip()) for i in range(n)]
-for i in range(len(task_difficulty)):
-  if not (1 <= task_difficulty[i] <= 100000):
-      print("Invalid input values")
-      exit()
+
+if any(not (1 <= x <= 100000) for x in task_difficulty):
+    print("Invalid input values")
+    exit()
 
 def minimum_time_to_finish_tasks(n, task_difficulty):
-    
-  task_difficulty.sort()
-  total_time = 0
-  for i in range(n):
-      total_time += task_difficulty[i] * task_difficulty[n - i - 1]
-  return total_time % (10**9 + 7)
+    task_difficulty.sort()
+    total_time = 0
+    for i in range(n):
+        total_time += task_difficulty[i] * task_difficulty[n - i - 1]
+    return total_time % 10007
 
 print(minimum_time_to_finish_tasks(n, task_difficulty))
